fc7c72f28cf56415696769bb600af8e996679814
Octavio <octavio.razo.verduzco@gmail.com>
2016-05-20 23:30:07 -0500

    landing page

---
+++ assets/js/controller.js
@@ -0,0 +1,27 @@
+angular
+    .module('apple', ["slick", "ngMaterial"])
+    .controller('webController', WebController)
+
+
+
+
+function WebController( $scope, $http) {
+  
+  $scope.m = true;
+
+  $http.get('http://jsonplaceholder.typicode.com/users').success(function(data) {
+            $scope.words = data;
+  }).error(function(error) {
+            console.log(error);
+  });
+
+
+  $scope.toggle1={show:false}
+  $scope.toggle2={show:false}
+  $scope.toggle2={show:false}
+  $scope.toggle2={show:false}
+  $scope.toggle5={show:false}
+  $scope.toggle6={show:false}
+  $scope.toggle7={show:false}
+
+};
